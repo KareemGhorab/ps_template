323b55dc5a82b7f62f64e7e284daedd36340c557
KareemGhorab <kareemghorab3@gmail.com>
2025-08-09 09:42:17 +0300

    fix(lib): easier gcd with recursion

--- lib.cpp
+++ lib.cpp
@@ -62,15 +62,16 @@ ll gcd(ll a, ll b) {
 }
 
 ll gcd(ll a, ll b, ll& x, ll& y) {
-  x = 1, y = 0;
-  ll x1 = 0, y1 = 1, a1 = a, b1 = b;
-  while (b1) {
-    int q = a1 / b1;
-    tie(x, x1) = make_tuple(x1, x - q * x1);
-    tie(y, y1) = make_tuple(y1, y - q * y1);
-    tie(a1, b1) = make_tuple(b1, a1 - q * b1);
+  if (b == 0) {
+    x = 1;
+    y = 0;
+    return a;
   }
-  return a1;
+  ll x1, y1;
+  ll d = gcd(b, a % b, x1, y1);
+  x = y1;
+  y = x1 - y1 * (a / b);
+  return d;
 }
 
 ll lcm(ll a, ll b) { return mul(a, divide(b, gcd(a, b))); }
